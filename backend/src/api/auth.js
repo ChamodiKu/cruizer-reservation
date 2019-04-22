495020575c7988c62b74af280ad338e73a145f43
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-22 15:31:32 +0530

    Implemented bcrypt password encryption

--- backend/src/api/auth.js
+++ backend/src/api/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const config = require('../../app.config')
 const verify = require('../auth/verify')
 const User = require('../model/User')
@@ -20,14 +21,16 @@ router.post('/signup', [verify.checkDuplicateUserNameOrEmail], function (req, re
   console.log('Sign up')
 
   if (!!req.body.username && !!req.body.email && !!req.body.password) {
-    const user = new User({
-      name: req.body.name || 'anonymous',
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email
-    })
-    user.save().then(_ => {
-      res.status(200).send({ message: 'Success, User created!' })
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+      const user = new User({
+        name: req.body.name || 'anonymous',
+        username: req.body.username,
+        password: hash,
+        email: req.body.email
+      })
+      user.save().then(_ => {
+        res.status(200).send({ message: 'Success, User created!' })
+      })
     }).catch(err => {
       console.log(err)
       res.status(500).send({ message: 'User creation error.' })
@@ -55,15 +58,17 @@ router.post('/signin', function (req, res) {
           })
         }
 
-        if (user.password === req.body.password) {
-          var token = jwt.sign({ id: user._id }, config.SECRET, {
-            expiresIn: 86400 // expires in 24 hours
-          })
+        bcrypt.compare(req.body.password, user.password).then(function (result) {
+          if (result) {
+            var token = jwt.sign({ id: user._id }, config.SECRET, {
+              expiresIn: 86400 // expires in 24 hours
+            })
 
-          res.status(200).send({ auth: true, accessToken: token })
-        } else {
-          return res.status(401).send({ auth: false, accessToken: null, reason: 'Invalid Password!' })
-        }
+            return res.status(200).send({ auth: true, accessToken: token })
+          } else {
+            return res.status(401).send({ auth: false, accessToken: null, reason: 'Invalid Password!' })
+          }
+        });
       })
   } else {
     res.status(400).send({ message: 'Missing fields' })
