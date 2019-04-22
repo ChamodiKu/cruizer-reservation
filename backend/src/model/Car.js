a0e57cd0b2c110e6fc33c7e02d726d397379875b
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-23 02:20:34 +0530

    Implemented car model

---
+++ backend/src/model/Car.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const carSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    createIndex: true,
+    required: true,
+    auto: true,
+  },
+  model: String,
+  vendor: String,
+  number: String,
+  milage: Number,
+  ownerId: mongoose.Schema.Types.ObjectId
+})
+
+module.exports = mongoose.model('Car', carSchema)
