ff538582ee462b3007e3bf88879087bc906842bd
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-22 14:43:27 +0530

    Change database configuration in config file

--- backend/app.config.js
+++ backend/app.config.js
@@ -3,12 +3,14 @@ var config = {};
 // Database configuration
 const db = {
   host: process.env.DB_HOST,
+  cluster: process.env.DM_CLUSTER,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   dbName: process.env.DB_NAME
 }
+
 console.log(db);
-config.DB = 'mongodb+srv://' + db.username + ':' + db.password + '@' + db.dbName + '-' + db.host + '/test?retryWrites=true'
+config.DB = 'mongodb+srv://' + db.username + ':' + db.password + '@' + db.cluster + '-' + db.host + '/' + db.dbName + '?retryWrites=true'
 
 // Secrets
 config.SECRET = process.env.SECRET;
