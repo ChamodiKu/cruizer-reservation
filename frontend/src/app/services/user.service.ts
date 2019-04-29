efa6c76631f074666f9a1da46c6a6cb3281f1cee
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-30 00:19:27 +0530

    Implemented current user state

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,7 +3,7 @@ import { AppConfig } from '../app-config';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user.dto';
-import { map, first } from 'rxjs/operators';
+import { map, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +17,28 @@ export class UserService {
     private http: HttpClient
   ) { }
 
-  current(): Observable<User> {
+  current(): User {
+    return JSON.parse(localStorage.getItem("current_user")) as User
+  }
+
+  collectCurrent(): Observable<User> {
     return this.http.get(this.currentUrl).pipe(
       first(),
-      map(res => res as User)
+      map(res => res as User),
+      tap(user => this.setUser(user))
     )
   }
+
+  removeCurrent() {
+    this.removeUser()
+  }
+
+  private setUser(response: User) {
+    localStorage.setItem("current_user", JSON.stringify(response))
+  }
+
+  private removeUser() {
+    localStorage.removeItem("current_user")
+  }
+
 }
