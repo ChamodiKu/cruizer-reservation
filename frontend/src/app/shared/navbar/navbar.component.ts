3018391642447897a60fd2d8d2cc70dd240b05c5
mihirukongahage <mihirukongahage@gmail.com>
2019-04-29 19:10:31 +0530

    sidebar

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { ROUTES } from '../sidebar/sidebar.component';
+//import { ROUTES } from '../sidebar/sidebar.component';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -18,18 +18,18 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.listTitles = ROUTES.filter(listTitle => listTitle);
+    //this.listTitles = ROUTES.filter(listTitle => listTitle);
   }
   getTitle(){
     var titlee = this.location.prepareExternalUrl(this.location.path());
     if(titlee.charAt(0) === '#'){
         titlee = titlee.slice( 2 );
     }
-    for(var item = 0; item < this.listTitles.length; item++){
+    /*for(var item = 0; item < this.listTitles.length; item++){
         if(this.listTitles[item].path === titlee){
             return this.listTitles[item].title;
         }
-    }
+    }*/
     return 'Dashboard';
   }
 
