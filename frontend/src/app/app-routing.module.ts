5ae4a30a8f0d6f2216ae56a9b9b27c6130e05269
mihirukongahage <mihirukongahage@gmail.com>
2019-04-29 18:28:22 +0530

    navbar and sidebar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+
 
 const routes: Routes = [];
 
@@ -7,4 +10,15 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+
 export class AppRoutingModule { }
