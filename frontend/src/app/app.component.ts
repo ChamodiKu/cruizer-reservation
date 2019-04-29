62b84f601ad7719e829b52c8afd15faa4aac91ac
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-29 22:32:41 +0530

    Refactored to login component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { AuthService } from './auth/auth.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,37 +6,5 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  public signInForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
-
-  public authorized: boolean
-
-  constructor(
-    private authService: AuthService
-  ) {
-    this.updateAuthorized()
-  }
-
-  onSignIn() {
-    const request = {
-      username: this.signInForm.controls['username'].value,
-      password: this.signInForm.controls['password'].value
-    }
-    this.authService.signIn(request).subscribe(() => {
-      this.updateAuthorized()
-    })
-  }
-
-  onSignOut() {
-    this.authService.signOut()
-    this.updateAuthorized()
-  }
-
-  private updateAuthorized() {
-    this.authorized = this.authService.isAuthorized()
-  }
 
 }
