fa4f2ab756d4c5518145bb98ecac405fcf5c3f95
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-29 21:56:01 +0530

    Implemented basic auth module

---
+++ frontend/src/app/auth/auth.dto.ts
@@ -0,0 +1,22 @@
+export interface SignInRequest {
+  username: string,
+  password: string
+}
+
+export interface SignInResponse {
+  auth: boolean,
+  accessToken: string,
+  expiresIn: string
+}
+
+export interface SignUpRequest {
+  firstname: string,
+  lastname: string,
+  username: string,
+  password: string,
+  email: string
+}
+
+export interface SignUpResponse {
+  message: string
+}
