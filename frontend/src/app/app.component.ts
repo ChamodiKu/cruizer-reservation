ab3be427e636b7eb8909c0e509441d4c2e1e7d8c
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-30 14:19:12 +0530

    Refactored nav bar component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { AuthService } from 'src/app/auth/auth.service';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-root',
@@ -9,5 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor(private _authService : AuthService){}
+  constructor(
+  ) { }
+
 }
