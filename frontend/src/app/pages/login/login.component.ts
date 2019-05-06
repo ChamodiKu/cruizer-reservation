46fcbda4332270c56be2fca8ce0ccea053444b52
mihirukongahage <mihirukongahage@gmail.com>
2019-05-06 19:10:43 +0530

    login validation

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { load } from '@angular/core/src/render3';
 
@@ -11,21 +11,42 @@ import { load } from '@angular/core/src/render3';
 })
 export class LoginComponent {
 
-  public signInForm = new FormGroup({
+  /*public signInForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl('')
-  });
+  });*/
 
-  error: String
-  loading: boolean
+  signInForm: FormGroup;
+
+  error: String;
+  loading: boolean;
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private formBuilder: FormBuilder
   ) { }
 
+  ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      //[value,validator]
+      username : [null,Validators.required],
+      password : [null,Validators.required]
+      /*address : this.formBuilder.group({}) */
+    });
+    
+  }
+
+  get username(){
+    const x = <FormGroup>this.signInForm;
+    return x.controls.username; 
+  }
+
   onSignIn() {
-    this.loading = true
+    console.log(this.signInForm);
+    this.loading = true;
+
+
     const request = {
       username: this.signInForm.controls['username'].value,
       password: this.signInForm.controls['password'].value
@@ -33,11 +54,14 @@ export class LoginComponent {
     this.authService.signIn(request).subscribe(() => {
       this.router.navigateByUrl('/portal');
     }, err => {
-      this.loading = false
-      console.log(err)
-      if (err.error.message)
-        this.error = err.error.message
+      this.loading = false;
+      console.log(err);
+      if (err.error.message) {
+        this.error = err.error.message;
+      }
     });
   }
 
 }
+
+
