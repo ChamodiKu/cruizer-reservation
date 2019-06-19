072b87cb92f87e44306674f615d3ef36b256ce97
mihirukongahage <mihirukongahage@gmail.com>
2019-06-19 15:26:35 +0530

    user details edit

--- frontend/src/app/pages/edit-userdetails/edit-userdetails.component.ts
+++ frontend/src/app/pages/edit-userdetails/edit-userdetails.component.ts
@@ -1,4 +1,15 @@
+import { UserService } from 'src/app/services/user.service';
+import { ReservationService } from './../../services/reservation.service';
+import {
+  Router,
+  ActivatedRoute,
+  ActivatedRouteSnapshot
+} from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { first, map, tap, flatMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/services/user.dto';
 
 @Component({
   selector: 'app-edit-userdetails',
@@ -6,10 +17,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-userdetails.component.scss']
 })
 export class EditUserdetailsComponent implements OnInit {
+  public userdetailsForm = new FormGroup({
+    username: new FormControl(''),
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    telephone: new FormControl(''),
+    address: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private ReservationService: ReservationService,
+    private userservice: UserService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  editId: string;
+
+  error: string;
+  loading: boolean;
+
+  onSubmit() {
+    const user: User = {
+      username: this.userdetailsForm.controls['username'].value,
+      firstname: this.userdetailsForm.controls['firstname'].value,
+      lastname: this.userdetailsForm.controls['lastname'].value,
+      tel: this.userdetailsForm.controls['telephone'].value,
+      address: this.userdetailsForm.controls['address'].value
+    };
+    let action: Observable<any>;
+    if (this.editId) {
+      action = this.userservice.update(this.editId, user);
+    } else {
+      action = this.userservice.create(user);
+    }
+    action.subscribe(
+      res => {
+        console.log(res);
+        this.router.navigate(['/dashboard']);
+      },
+      err => {
+        this.loading = false;
+        if (err.error.message) {
+          this.error = err.error.message;
+        }
+      }
+    );
   }
-
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import { AppConfig } from '../app-config';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from './user.dto';
+import { User, UserUpdate } from './user.dto';
 import { map, first, tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { ApiResponse } from '../api/api.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
   // API Endpoints
   private currentUrl = AppConfig.apiUrl + '/users/current';
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient, private _router: ActivatedRoute) {}
 
   current(): User {
     return JSON.parse(localStorage.getItem('current_user')) as User;
@@ -41,4 +40,17 @@ export class UserService {
     localStorage.removeItem('current_user');
   }
 
+  create(service: User): Observable<ApiResponse> {
+    return this.http.post(this.currentUrl, service).pipe(
+      first(),
+      map(res => res as ApiResponse)
+    );
+  }
+
+  update(serviceId: string, update: UserUpdate): Observable<ApiResponse> {
+    return this.http.put(this.currentUrl + '/' + serviceId, update).pipe(
+      first(),
+      map(res => res as ApiResponse)
+    );
+  }
 }
