dc2e81bef55cb047872c1c89ea8e574d27a540d2
Sankha Rathnayake <43112439+sankharr@users.noreply.github.com>
2019-07-05 07:42:45 +0530

    Done

--- frontend/src/app/pages/gst-edit/gst-edit.component.ts
+++ frontend/src/app/pages/gst-edit/gst-edit.component.ts
@@ -25,6 +25,27 @@ export class GstEditComponent implements OnInit {
       job_id: ['', Validators.required ],
       technician_id: ['', Validators.required ],
       date_serviced: ['', Validators.required ],
+      vehicle_no: ['', Validators.required ],
+      make: ['', Validators.required ],
+      model: ['', Validators.required ],
+      owner_name: ['', Validators.required ],
+      mileage: ['', Validators.required ],
+      lights_indicators: ['', Validators.required ],
+      horn: ['', Validators.required ],
+      wipers: ['', Validators.required ],
+      side_mirror: ['', Validators.required ],
+      power_windows: ['', Validators.required ],
+      brake_fluid: ['', Validators.required ],
+      engine_coolant: ['', Validators.required ],
+      drive_belt: ['', Validators.required ],
+      ac_filter: ['', Validators.required ],
+      air_filter: ['', Validators.required ],
+      front_brakes: ['', Validators.required ],
+      rear_brakes: ['', Validators.required ],
+      engine_oil: ['', Validators.required ],
+      oil_filter: ['', Validators.required ],
+      shocks: ['', Validators.required ],
+      tyre_pressure: ['', Validators.required ],
       body_wash: ['', Validators.required ]
     });
   }
@@ -44,9 +65,27 @@ export class GstEditComponent implements OnInit {
     });
   }
 
-  updateBusiness(job_id, technician_id, date_serviced, body_wash) {
+  updateBusiness(job_id, technician_id, date_serviced, vehicle_no,make,model,owner_name,mileage) {
+    let bodyWash = this.angForm.get('body_wash').value;
+    let lightsIndicators = this.angForm.get('lights_indicators').value;
+    let horN = this.angForm.get('horn').value;
+    let wiperS = this.angForm.get('wipers').value; 
+    let sideMirror = this.angForm.get('side_mirror').value;
+    let powerWindows = this.angForm.get('power_windows').value;
+    let brakeFluid = this.angForm.get('brake_fluid').value;
+    let engineCoolant = this.angForm.get('engine_coolant').value;
+    let driveBelt = this.angForm.get('drive_belt').value;
+    let acFilter = this.angForm.get('ac_filter').value;
+    let airFilter = this.angForm.get('air_filter').value;
+    let frontBrakes = this.angForm.get('front_brakes').value;
+    let rearBrakes = this.angForm.get('rear_brakes').value;
+    let engineOil = this.angForm.get('engine_oil').value;
+    let oilFilter = this.angForm.get('oil_filter').value;
+    let shockS = this.angForm.get('shocks').value;
+    let tyrePressure = this.angForm.get('tyre_pressure').value;
     this.route.params.subscribe(params => {
-      this.bs.updateBusiness(job_id, technician_id, date_serviced, body_wash, params['id']).subscribe((data: string) => {
+      this.bs.updateBusiness(job_id, technician_id, date_serviced, vehicle_no,make,model,owner_name,mileage,lightsIndicators,
+        horN,wiperS,sideMirror,powerWindows,brakeFluid,engineCoolant,driveBelt,acFilter, airFilter,frontBrakes,rearBrakes,engineOil,oilFilter,shockS,tyrePressure, bodyWash, params['id']).subscribe((data: string) => {
         console.log(data);
         this.router.navigate(['business']);
       });
